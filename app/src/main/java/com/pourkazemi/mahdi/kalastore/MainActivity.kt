8a3576f7c3fac61eedad557275021b18d2a563b1
mahdiPourkazemi <mahdi@pourkazemi.com>
2022-05-23 11:24:31 +0430

    Initialize button navigation and colors
    Took 1 hour 6 minutes

--- app/src/main/java/com/pourkazemi/mahdi/kalastore/MainActivity.kt
+++ app/src/main/java/com/pourkazemi/mahdi/kalastore/MainActivity.kt
@@ -2,10 +2,19 @@ package com.pourkazemi.mahdi.kalastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.pourkazemi.mahdi.kalastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val mNavHost =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(mNavHost.navController)
+
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/pourkazemi/mahdi/kalastore/ui/navigation/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.pourkazemi.mahdi.kalastore.ui.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pourkazemi.mahdi.kalastore.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
