f404f8a87192a63f06b43f302fec1e54690a7b10
mahdiPourkazemi <mahdi@pourkazemi.com>
2022-05-25 20:46:20 +0430

    add special type list and requirement
    Took 2 hours 2 minutes

--- app/src/main/java/com/pourkazemi/mahdi/kalastore/ui/viewmodels/CategoryViewModel.kt
+++ app/src/main/java/com/pourkazemi/mahdi/kalastore/ui/viewmodels/CategoryViewModel.kt
@@ -20,15 +20,26 @@ class CategoryViewModel @Inject constructor(
         MutableStateFlow(ResultWrapper.Loading)
     val categoryList = _categoryList.asStateFlow()
 
+    private val _categorySpecialKalaList: MutableStateFlow<ResultWrapper<List<Kala>>> =
+        MutableStateFlow(ResultWrapper.Loading)
+    val categorySpecialKalaList = _categorySpecialKalaList.asStateFlow()
+
     init {
         getListKalaCategory()
     }
 
-    fun getListKalaCategory() {
+   private fun getListKalaCategory() {
         viewModelScope.launch {
             repository.getListKalaCategory().collect {
                 _categoryList.emit(it)
             }
         }
     }
+   fun getSpecialListKala(category:String){
+      viewModelScope.launch {
+          repository.getSpecialCategoryListKala(category).collect{
+             _categorySpecialKalaList.emit(it)
+          }
+      }
+   }
 }
